d921bb14c128794f833c384a4d58c7582c3d27f4
Alberto <ajs5433@rit.edu>
2017-03-30 17:32:59 -0700

    created the basics of the user class

---
+++ Program/src/user.java
@@ -0,0 +1,12 @@
+public class user {
+	int name, lastname, 
+	username, password,
+	number, symbol, 
+	PAO1_character, PAO1_action, PAO1_object,
+	PAO2_character, PAO2_action, PAO2_object,
+	registration_date;
+	
+	public user(){
+		
+	}
+}
