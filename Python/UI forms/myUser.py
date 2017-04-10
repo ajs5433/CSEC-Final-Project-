5d997b38b85bbfe094fdcf82c5d95f7a15f125e5
Alberto <ajs5433@rit.edu>
2017-04-10 09:48:58 -0700

    still working on the users info

--- Python/UI forms/myUser.py
+++ Python/UI forms/myUser.py
@@ -2,7 +2,6 @@ import datetime
 import time
 import json
 import os
-#from pathlib import Path
 
 
 filepath = 'A:/Desktop/myProject/Project Files/userBook.txt'
@@ -44,49 +43,63 @@ class createUser():
 
     def save(self):
         #print("Saving user")
-
+        create = False;
         self.last_login = self.first_login,
-        #my_file = Path(filepath)
-        #if my_file.is_file():
+
+        #check if file exists and if its empty
         if (os.path.isfile(filepath)):
             if(os.stat(filepath).st_size != 0):
                 with open(filepath, "r") as f:
-                    oldfile = f.read()
-                    oldbook = json.loads(oldfile)
+                    dataString = f.read()
+                    dataBook = json.loads(dataString)
 
-                    if self.username in oldbook:
+                    if self.username in dataString:
                         print("there's already a user named "+ self.username)
+                        create = False
                     else:
                         print('no user named ' + self.username)
+                        create = True
+            if(create):
+                self.userBook[self.username] = {
+                    'name:': self.name,
+                    'last name': self.lastName,
+                    'password type': self.passwordType,
+                    'password': self.password,
+                    'PAO1_person': self.PAO1_person,
+                    'PAO1_action': self.PAO1_action,
+                    'PAO1_object': self.PAO1_object,
+                    'PAO1_full': self.PAO1_full,
+                    'PAO2_person': self.PAO2_person,
+                    'PAO2_action': self.PAO2_action,
+                    'PAO2_object': self.PAO2_object,
+                    'PAO2_full': self.PAO2_full,
+                    'number': self.number,
+                    'symbol': self.symbol,
+                    'setup time': self.setup_time,
+                    'first login': self.first_login,
+                    'last login': self.last_login
+                }
 
-        self.userBook[self.username] = {
-            'name:': self.name,
-            'last name': self.lastName,
-            'password type': self.passwordType,
-            'password': self.password,
-            'PAO1_person': self.PAO1_person,
-            'PAO1_action': self.PAO1_action,
-            'PAO1_object': "JonnyBoTest",#self.PAO1_object,
-            'PAO1_full': self.PAO1_full,
-            'PAO2_person': self.PAO2_person,
-            'PAO2_action': self.PAO2_action,
-            'PAO2_object': self.PAO2_object,
-            'PAO2_full': self.PAO2_full,
-            'number': self.number,
-            'symbol': self.symbol,
-            'setup time': self.setup_time,
-            'first login': self.first_login,
-            'last login': self.last_login
-        }
+                dataBook.update(self.userBook)
 
-        with open(filepath, "w") as f:
-            newbook = json.dumps(self.userBook)
-            f.write(newbook)
+                with open(filepath, "w") as f:
+                    newbook = json.dumps(dataBook)
+                    f.write(newbook)
 
         #json.dumps(userBook)
 
+    def update_file(self, field,update):
+
+        with open(filepath, "r") as json_file:
+            with open(filepath, 'r') as f:
+                json_data = json.load(f)
+                json_data[field] = update
+
+            with open('my_file.json', 'w') as f:
+                f.write(json.dumps(json_data))
+
 if __name__== "__main__":
-    myuser = createUser("Jonny", "Bo", "Test")
+    myuser = createUser("gotta", "Go888", "Test")
     myuser.save()
 
 
